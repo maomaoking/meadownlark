c2af8b5cde1a2d15a4e8a37e260aad82cfdd6b42
Zhang Yumou <yumou.zhang@shenma-inc.com>
2015-12-26 12:50:13 +0800

    add

--- meadowlark.js
+++ meadowlark.js
@@ -7,33 +7,45 @@ var app = express();
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
-
-
 app.set('port', process.env.PORT || 3000);
 
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
 
+
+//路由开始
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-})
+    //res.type('text/plain');
+    //res.send('Meadowlark Travel');
+    res.render('home');
+});
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-})
+    //res.type('text/plain');
+    //res.send('About Meadowlark Travel');
+    res.render('about');
+});
 //404
 app.use(function (req, res) {
-    res.type('text/plain');
+    //res.type('text/plain');
     res.status(404);
-    res.send('404 - not found');
+    //res.send('404 - not found');
+    res.render('404');
 });
 
 //500
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
+    //res.type('text/plain');
     res.status(500);
-    res.send('500 - server error');
+    //res.send('500 - server error');
+    res.render('500');
 });
 
 
