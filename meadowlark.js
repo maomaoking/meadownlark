ea24e8c1348bbb45b77790a0b85029b16a5dca9b
Zhang Yumou <yumou.zhang@shenma-inc.com>
2015-12-26 12:22:16 +0800

    add

--- meadowlark.js
+++ meadowlark.js
@@ -4,6 +4,10 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
 
 app.set('port', process.env.PORT || 3000);
 
