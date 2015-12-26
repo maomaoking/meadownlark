5f4ece6216d25a060a479043e6eea6a6d979fa44
Zhang Yumou <yumou.zhang@shenma-inc.com>
2015-12-26 12:00:54 +0800

    add

--- meadowlark.js
+++ meadowlark.js
@@ -7,8 +7,18 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
+})
+
+app.get('/about', function (req, res) {
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+})
 //404
-app.ues(function (req, res) {
+app.use(function (req, res) {
     res.type('text/plain');
     res.status(404);
     res.send('404 - not found');
@@ -22,11 +32,8 @@ app.use(function (err, req, res, next) {
     res.send('500 - server error');
 });
 
-//200
-//app.use(function (req, res) {
-//
-//})
+
 app.listen(app.get('port'), function () {
     console.log( 'Express started on http://localhost:' +
         app.get('port')+ '; press Ctrl-C to terminate.');
-})
+});
