85c0c35176f3878825d30a90aba8254a67f89989
Zhang Yumou <yumou.zhang@shenma-inc.com>
2015-12-26 23:46:36 +0800

    add

--- meadowlark.js
+++ meadowlark.js
@@ -34,6 +34,13 @@ app.get('/about', function (req, res) {
             pageTestScript: '/qa/tests-about.js'
     });
 });
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
+});
+
 //404
 app.use(function (req, res) {
     //res.type('text/plain');
